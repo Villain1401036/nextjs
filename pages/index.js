6b293faf24c5e332ac4562917e1434e8210df93f
Villain1401036 <42384322+Villain1401036@users.noreply.github.com>
2020-08-29 22:09:32 +0530

    next1

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Main() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Spook</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
