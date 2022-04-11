86d65091aff5825bf2ee223e59463c3294075f34
Villain1401036 <42384322+Villain1401036@users.noreply.github.com>
2022-04-12 00:59:26 +0530

    .

--- next.config.js
+++ next.config.js
@@ -15,10 +15,3 @@ module.exports = withPWA({
   },
 });
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
-module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
-})
