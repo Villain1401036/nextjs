f8123a73a28facb7ba30d71b07c703e72f781563
Villain1401036 <42384322+Villain1401036@users.noreply.github.com>
2022-06-22 23:09:57 +0530

    .

--- pages/_app.js
+++ pages/_app.js
@@ -180,6 +180,24 @@ function MyApp({ Component, pageProps }) {
         }
 
 
+        const messaging = getMessaging(app);
+        
+        getToken(messaging, { vapidKey: WEB_PUSH_PUBLIC_KEY }).then((currentToken) => {
+          if (currentToken) {
+            console.log(currentToken);
+            // Send the token to your server and update the UI if necessary
+            // ...
+          } else {
+            // Show permission request UI
+            console.log('No registration token available. Request permission to generate one.');
+            // ...
+          }
+        }).catch((err) => {
+          console.log('An error occurred while retrieving token. ', err);
+          // ...
+        });
+        
+      
 
         setLoaded(true)  
          
